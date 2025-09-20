733980cd806f6c4adf967480858859f3eb60d635
NateOrlowski06 <nateorlowski@protonmail.com>
2025-09-19 19:29:12 -0600

    Added all code needed to plot 30mps data in a second figure. The code does not run right now because we do not have the data for 30mps. When we have the data for 30 ill upload it and push to main so it runs

--- Airfoil_Analysis_Main.m
+++ Airfoil_Analysis_Main.m
@@ -35,7 +35,7 @@ for i = 1:numFiles
 end
 fileNames15 = string(fileNames15); % Convert to char array from cell array
 AoA_Count15 = length(AoA15); %Counts number of AoAs tested
-%{
+
 fileLoc = '30 mps Data Files/';
 list = dir([fileLoc, '*AoA*']); % This lists all files in fileLoc with 'WTData' in the file name
 numFiles = length(list);
@@ -45,23 +45,22 @@ for i = 1:numFiles
 end
 fileNames30 = string(fileNames30); % Convert to char array from cell array
 AoA_Count30 = length(AoA30); %Counts number of AoAs tested
-%}
-%% Ingest Data Files and Data Conditioning
-% Averaging Raw Data Samples for each Velocity & AOA Tested
-% Derived Values for each Velocity & AoA tested
-   % Average Test Section Static Pressure (done by student code)
-   % Average Airfoil Port Local Velocity (done by student code)
-   % Average Airfoil Port Local Static Pressure (done by student code)
-% Initialize Storage Arrays
-Data15 = zeros(numFiles,25); %Conditioned wind tunnel data file 15 m/s
-%%%%Data30 = zeros(numFiles,25); %Conditioned wind tunnel data file 30 m/s
-%% Ingest and Condition Data
-% Averaging Raw Data Samples for each Velocity & AOA Tested
-% Derived Values for each Velocity & AoA tested
-   % Average Test Section Static Pressure (done by student code)
-   % Average Airfoil Port Local Velocity (done by student code)
-   % Average Airfoil Port Local Static Pressure (done by student code)
 
+
+Data15 = zeros(numFiles,25); %Conditioned wind tunnel data file 15 m/s
+Data30 = zeros(numFiles,25); %Conditioned wind tunnel data file 30 m/s
+
+
+
+
+
+
+
+%%%%Our code begins%%%%%%%
+
+
+
+%%Convert raw 15mps data files into Data15
 for j = 1:numFiles
    RawData15 = readmatrix(fileNames15(j),'NumHeaderLines',1); % load the data
    %Condition data
@@ -74,11 +73,7 @@ for j = 1:numFiles
    Data15(j,7) = Data15(j,3) - Data15(j,6); %Calculates test section static pressure by calculating Po - q
    %Calculate airfoil port static pressures from measured differential
    %pressues (P_port - P_static_test) & sets columns 8 - 24 as airfoil static port pressure values (calculated)
-   %To get the port static pressure, we have to subtract out the static
-   %pressure in  the atmosphere that was calculated in data15(j,7)
-   %Since scanivalve ports are differential pressure from the
-   %test section, we dont need to subtract out anything and the TE should
-   %be 0
+  
    Data15(j,8) = mean(RawData15(:,15)); %Airfoil scanivalve port 1
    Data15(j,9) = mean(RawData15(:,16)); %Airfoil scanivalve port 2
    Data15(j,10) = mean(RawData15(:,17)); %Airfoil scanivalve port 3
@@ -99,58 +94,66 @@ for j = 1:numFiles
    Data15(j,25) = Data15(j,8); %Repeats port 1 (leading edge)
 end
 Data15 = sortrows(Data15,1); %Sorts data by increasing AoA
-%{
+
+
+%Convert raw 30mps data into Data30 
 for j = 1:numFiles
    RawData30 = readmatrix(fileNames30(j),'NumHeaderLines',1); % load the data
    %Condition data
    Data30(j,1) = AoA30(j); %Sets AoA (deg) as 1st column
-   Data30(j,2) = ; %Sets mean of velocity measurements as 2nd column
-   Data30(j,3) = ; %Sets mean of total pressure (atmosphere) as 3rd column
-   Data30(j,4) = ; %Sets mean of temperature (atmosphere) as 4th column
-   Data30(j,5) = ; %Sets mean of calculaed density (atmosphere) as 5th column
-   Data30(j,6) = ; %Sets mean of test section dynamic pressure as 6th column
-   Data30(j,7) = ; %Calculates test section static pressure by calculating Po - q
+   Data30(j,2) = mean(RawData30(:,4)); %Sets mean of velocity measurements as 2nd column
+   Data30(j,3) = mean(RawData30(:,2)); %Sets mean of total pressure (atmosphere) as 3rd column
+   Data30(j,4) = mean(RawData30(:,1)); %Sets mean of temperature (atmosphere) as 4th column
+   Data30(j,5) = mean(RawData30(:,3)); %Sets mean of calculaed density (atmosphere) as 5th column
+   Data30(j,6) = mean(RawData30(:,5)); %Sets mean of test section dynamic pressure as 6th column
+   Data30(j,7) = Data30(j,3) - Data30(j,6); %Calculates test section static pressure by calculating Po - q
    %Calculate airfoil port static pressures from measured differential
-   %pressues (P_port - P_static_test) & sets columns 8 - 24 as airfoil static port pressure values calculated
-   Data30(j,8) = ; %Airfoil scanivalve port 1
-   Data30(j,9) = ; %Airfoil scanivalve port 2
-   Data30(j,10) = ; %Airfoil scanivalve port 3
-   Data30(j,11) = ; %Airfoil scanivalve port 4
-   Data30(j,12) = ; %Airfoil scanivalve port 5
-   Data30(j,13) = ; %Airfoil scanivalve port 6
-   Data30(j,14) = ; %Airfoil scanivalve port 7
-   Data30(j,15) = ; %Airfoil scanivalve port 8
-   Data30(j,16) = ; %Airfoil scanivalve port 9
-   Data30(j,17) = ; %TE  - Assume Airfoil trailing edge pressure = test section static pressure
-   Data30(j,18) = ; %Airfoil scanivalve port 10
-   Data30(j,19) = ; %Airfoil scanivalve port 11
-   Data30(j,20) = ; %Airfoil scanivalve port 12
-   Data30(j,21) = ; %Airfoil scanivalve port 13
-   Data30(j,22) = ; %Airfoil scanivalve port 14
-   Data30(j,23) = ; %Airfoil scanivalve port 15
-   Data30(j,24) = ; %Airfoil scanivalve port 16
-   Data30(j,25) = ; %Repeats port 1 (leading edge)
+   %pressues (P_port - P_static_test) & sets columns 8 - 24 as airfoil static port pressure values (calculated)
+  
+   Data30(j,8) = mean(RawData30(:,15)); %Airfoil scanivalve port 1
+   Data30(j,9) = mean(RawData30(:,16)); %Airfoil scanivalve port 2
+   Data30(j,10) = mean(RawData30(:,17)); %Airfoil scanivalve port 3
+   Data30(j,11) = mean(RawData30(:,18)); %Airfoil scanivalve port 4
+   Data30(j,12) = mean(RawData30(:,19)); %Airfoil scanivalve port 5
+   Data30(j,13) = mean(RawData30(:,20)); %Airfoil scanivalve port 6
+   Data30(j,14) = mean(RawData30(:,21)); %Airfoil scanivalve port 7
+   Data30(j,15) = mean(RawData30(:,22)); %Airfoil scanivalve port 8
+   Data30(j,16) = mean(RawData30(:,23)); %Airfoil scanivalve port 9
+   Data30(j,17) = 0; %TE - Assume Airfoil trailing edge pressure = test section static pressure
+   Data30(j,18) = mean(RawData30(:,24)); %Airfoil scanivalve port 10
+   Data30(j,19) = mean(RawData30(:,25)); %Airfoil scanivalve port 11
+   Data30(j,20) = mean(RawData30(:,26)); %Airfoil scanivalve port 12
+   Data30(j,21) = mean(RawData30(:,27)); %Airfoil scanivalve port 13
+   Data30(j,22) = mean(RawData30(:,28)); %Airfoil scanivalve port 14
+   Data30(j,23) = mean(RawData30(:,29)); %Airfoil scanivalve port 15
+   Data30(j,24) = mean(RawData30(:,30)); %Airfoil scanivalve port 16
+   Data30(j,25) = Data30(j,8); %Repeats port 1 (leading edge)
 end
 Data30 = sortrows(Data30,1); %Sorts data by increasing AoA
-%}
 
 
 
-%% Determine Forces & Analyze Results
-% Pressure Distribution for each Velocity & AoA tested (done by student code)
 
-
-Calculated_Values = zeros(30,5);
+%Chord length calculation
 dX = Ports(1,2) - Ports(10,2);
 dY = Ports(1,3) - Ports(10,3);
 ChordLength = sqrt(dX^2 + dY^2);
 
-%Loops over every AoA
+
+
+
+
+%%%%%%%%%%%%%%%15 meters per second calculations%%%%%%%%%%%%%%%%%%%%%
+Calculated_Values15 = zeros(30,5);
+
+%Force calculations
+%Loops over every AoA for 15m/s
 for i = 1:30
    N_upper_sum = 0;
    A_upper_sum = 0;
    N_lower_sum = 0;
    A_lower_sum = 0;
+
    %Loops over upper surface, from leading edge to trailing edge
    for j = 8:16
        Index = j-7; % 1...9
@@ -170,49 +173,47 @@ for i = 1:30
 
    %Storing all calculated values into array
    %Order of storage: AoA, Fa, Fn, Fl, Cl
-   Calculated_Values(i,1) = Data15(i,1);
-   Calculated_Values(i,2) = AxialForce;
-   Calculated_Values(i,3) = NormalForce;
+   Calculated_Values15(i,1) = Data15(i,1);
+   Calculated_Values15(i,2) = AxialForce;
+   Calculated_Values15(i,3) = NormalForce;
 
    %Lift force calculations
    Lift_Force = NormalForce * cosd(Data15(i,1)) - AxialForce * sind(Data15(i,1));
-   Calculated_Values(i,4) = Lift_Force;
+   Calculated_Values15(i,4) = Lift_Force;
 
    %Coefficient of Lift Calculation
    FreeStreamDynPressure = 0.5 * Data15(i,5) * Data15(i,2)^2;
    Coefficient_lift = Lift_Force / (FreeStreamDynPressure * ChordLength);
-   Calculated_Values(i,5) = Coefficient_lift;
+   Calculated_Values15(i,5) = Coefficient_lift;
 end
 
 
 
 
-% Calculation for Velocity at each port
-VelUpper = zeros(9, 31);
-VelLower = zeros(8, 31);
-CpUpper = zeros(9,31);
-CpLower = zeros(8,31);
+% Arrays to store Coefficient of pressure and velocities at each port
+VelUpper15 = zeros(9, 31);
+VelLower15 = zeros(8, 31);
+CpUpper15 = zeros(9,31);
+CpLower15 = zeros(8,31);
 
 
-
-
-%Calculates x/c for each point along upper surface
+%Calculates normalized chord (x/c) for each point along upper and lower surface
 for i = 1:9
    x_pos = Ports(i, 2);
    nChord = x_pos / ChordLength;
-   VelUpper(i, 1) = nChord;
-   CpUpper(i,1) = nChord;
+   VelUpper15(i, 1) = nChord;
+   CpUpper15(i,1) = nChord;
 end
-%Calculates x/c for each point along lower surface
 for i = 1:8
    index = 19 - i;
    x_pos = Ports(index, 2);
    nChord = x_pos / ChordLength;
-   VelLower(i, 1) = nChord;
-   CpLower(i,1) = nChord;
+   VelLower15(i, 1) = nChord;
+   CpLower15(i,1) = nChord;
 end
 
 
+%Calculations for velocity over each port
 %Loops over every AoA
 for i = 1:30
 
@@ -222,10 +223,10 @@ for i = 1:30
 
        %Velocity calculation at each port
        Velocity = sqrt((2 * (abs(Data15(i, index))) / Data15(i, 5)));
-       VelUpper(j, i+1) = Velocity;
+       VelUpper15(j, i+1) = Velocity;
 
        %Coefficient of pressure calculation at each port
-       CpUpper(j,i+1) = Data15(i,index) / Data15(i,6);
+       CpUpper15(j,i+1) = Data15(i,index) / Data15(i,6);
    end
 
    %Loops from leading edge to last port on lower surface
@@ -234,16 +235,120 @@ for i = 1:30
 
        %Velocity calculation at each port
        Velocity = sqrt((2 * (abs(Data15(i, index))) / Data15(i, 5)));
-       VelLower(19 - j, i+1) = Velocity;
+       VelLower15(19 - j, i+1) = Velocity;
 
        %Coefficient of pressure calculation at each port
-       CpLower(19-j,i+1) = Data15(i,index) / Data15(i,6);
+       CpLower15(19-j,i+1) = Data15(i,index) / Data15(i,6);
   
    end
 end
 
+[MinCoefficient15, ZeroLiftIndex15] = min(abs(Calculated_Values15(: , 5)));
 
-[MinCoefficient, ZeroLiftIndex] = min(abs(Calculated_Values(: , 5)));
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%30 meters per second calculations copy pasted from 15 changed to 30
+
+Calculated_Values30 = zeros(30,5);
+
+%Force calculations
+%Loops over every AoA for 30m/s
+for i = 1:30
+   N_upper_sum = 0;
+   A_upper_sum = 0;
+   N_lower_sum = 0;
+   A_lower_sum = 0;
+
+   %Loops over upper surface, from leading edge to trailing edge
+   for j = 8:16
+       Index = j-7; % 1...9
+       N_upper_sum = N_upper_sum  - 0.5*(Data30(i,j) + Data30(i,j+1)) * Segments(Index,1);
+       A_upper_sum = A_upper_sum  + 0.5*(Data30(i,j) + Data30(i,j+1)) * Segments(Index,2);
+   end
+
+   %Loops over lower surface from leading edge to trailing edge
+   for j = 25:-1:18
+       Index = -1*j + 35;
+       N_lower_sum = N_lower_sum + 0.5*(Data30(i,j) + Data30(i,j-1)) * Segments(Index,1);
+       A_lower_sum = A_lower_sum - 0.5*(Data30(i,j) + Data30(i,j-1)) * Segments(Index,2);
+   end
+  
+   NormalForce = N_upper_sum + N_lower_sum;
+   AxialForce  = A_upper_sum + A_lower_sum;
+
+   %Storing all calculated values into array
+   %Order of storage: AoA, Fa, Fn, Fl, Cl
+   Calculated_Values30(i,1) = Data30(i,1);
+   Calculated_Values30(i,2) = AxialForce;
+   Calculated_Values30(i,3) = NormalForce;
+
+   %Lift force calculations
+   Lift_Force = NormalForce * cosd(Data30(i,1)) - AxialForce * sind(Data30(i,1));
+   Calculated_Values30(i,4) = Lift_Force;
+
+   %Coefficient of Lift Calculation
+   FreeStreamDynPressure = 0.5 * Data30(i,5) * Data30(i,2)^2;
+   Coefficient_lift = Lift_Force / (FreeStreamDynPressure * ChordLength);
+   Calculated_Values30(i,5) = Coefficient_lift;
+end
+
+
+
+
+%Arrays to store coefficient of pressure  and velocities at each port
+VelUpper30 = zeros(9, 31);
+VelLower30 = zeros(8, 31);
+CpUpper30 = zeros(9,31);
+CpLower30 = zeros(8,31);
+
+
+%Calculates normalized chord (x/c) for each point along upper and lower surface
+for i = 1:9
+   x_pos = Ports(i, 2);
+   nChord = x_pos / ChordLength;
+   VelUpper30(i, 1) = nChord;
+   CpUpper30(i,1) = nChord;
+end
+for i = 1:8
+   index = 19 - i;
+   x_pos = Ports(index, 2);
+   nChord = x_pos / ChordLength;
+   VelLower30(i, 1) = nChord;
+   CpLower30(i,1) = nChord;
+end
+
+%Calculations for velocity over each port
+%Loops over every AoA
+for i = 1:30
+
+   %Loops from leading edge to last port on upper surface
+   for j = 1:9
+       index = j + 7;
+
+       %Velocity calculation at each port
+       Velocity = sqrt((2 * (abs(Data30(i, index))) / Data30(i, 5)));
+       VelUpper30(j, i+1) = Velocity;
+
+       %Coefficient of pressure calculation at each port
+       CpUpper30(j,i+1) = Data30(i,index) / Data30(i,6);
+   end
+
+   %Loops from leading edge to last port on lower surface
+   for j = 18:-1:11
+       index = j + 7;
+
+       %Velocity calculation at each port
+       Velocity = sqrt((2 * (abs(Data30(i, index))) / Data30(i, 5)));
+       VelLower30(19 - j, i+1) = Velocity;
+
+       %Coefficient of pressure calculation at each port
+       CpLower30(19-j,i+1) = Data30(i,index) / Data30(i,6);
+  
+   end
+end
+
+[MinCoefficient30, ZeroLiftIndex30] = min(abs(Calculated_Values30(: , 5)));
 
 
 
@@ -253,19 +358,14 @@ end
 NACA = readmatrix("ClarkY14_NACA_TR628.xlsx");
 
 
-
-
-
-
-
-%% Plots
+%% Plots for 15 m/s
 % Velocity vs normalized chord (x/c)
 % 6 degrees
 figure;
 subplot(3,3,1);
-plot(VelUpper(1:9, 1), VelUpper(1:9, 22), 'r')
+plot(VelUpper15(1:9, 1), VelUpper15(1:9, 22), 'r')
 hold on
-plot(VelLower(1:8, 1), VelLower(1:8, 22), 'b')
+plot(VelLower15(1:8, 1), VelLower15(1:8, 22), 'b')
 legend('Upper Velocity', 'Lower Velocity')
 title('Velocity vs Normalized Chord')
 subtitle('6 Degree AoA 15 m/s')
@@ -273,9 +373,9 @@ hold off
 
 % 0 Lift
 subplot(3, 3, 2);
-plot(VelUpper(1:9, 1), VelUpper(1:9, ZeroLiftIndex + 1), 'r')
+plot(VelUpper15(1:9, 1), VelUpper15(1:9, ZeroLiftIndex15 + 1), 'r')
 hold on
-plot(VelLower(1:8, 1), VelLower(1:8, ZeroLiftIndex + 1), 'b')
+plot(VelLower15(1:8, 1), VelLower15(1:8, ZeroLiftIndex15 + 1), 'b')
 legend('Upper Velocity', 'Lower Velocity')
 title('Velocity vs Normalized Chord')
 subtitle('Zero Lift 15 m/s')
@@ -284,24 +384,21 @@ hold off
 
 % Stall 10 degrees 
 subplot(3, 3, 3);
-plot(VelUpper(1:9, 1), VelUpper(1:9, 26), 'r')
+plot(VelUpper15(1:9, 1), VelUpper15(1:9, 26), 'r')
 hold on
-plot(VelLower(1:8, 1), VelLower(1:8, 26), 'b')
+plot(VelLower15(1:8, 1), VelLower15(1:8, 26), 'b')
 legend('Upper Velocity', 'Lower Velocity')
 title('Velocity vs Normalized Chord')
 subtitle('Stall Angle 15 m/s')
 hold off
 
 
-
-
-
 % Coefficient of Pressure vs normalized chord (x/c)
 %6 degrees 
 subplot(3,3,4);
-plot(CpUpper(1:9, 1), CpUpper(1:9, 22), 'r')
+plot(CpUpper15(1:9, 1), CpUpper15(1:9, 22), 'r')
 hold on
-plot(CpLower(1:8, 1), CpLower(1:8, 22), 'b')
+plot(CpLower15(1:8, 1), CpLower15(1:8, 22), 'b')
 legend('Upper Cp', 'Lower Cp')
 title('Coefficient of Pressure vs Normalized Chord')
 subtitle('6 Degree AoA 15 m/s')
@@ -309,31 +406,110 @@ hold off
 
 %Zero lift
 subplot(3,3,5);
-plot(CpUpper(1:9, 1), CpUpper(1:9, ZeroLiftIndex + 1), 'r')
+plot(CpUpper15(1:9, 1), CpUpper15(1:9, ZeroLiftIndex15 + 1), 'r')
 hold on
-plot(CpLower(1:8, 1), CpLower(1:8, ZeroLiftIndex + 1), 'b')
+plot(CpLower15(1:8, 1), CpLower15(1:8, ZeroLiftIndex15 + 1), 'b')
 legend('Upper Cp', 'Lower Cp')
 title('Coefficient of Pressure vs Normalized Chord')
 subtitle('Zero Lift 15 m/s')
 
 %Stall
 subplot(3,3,6);
-plot(CpUpper(1:9, 1), CpUpper(1:9, 26), 'r')
+plot(CpUpper15(1:9, 1), CpUpper15(1:9, 26), 'r')
 hold on
-plot(CpLower(1:8, 1), CpLower(1:8, 26), 'b')
+plot(CpLower15(1:8, 1), CpLower15(1:8, 26), 'b')
 legend('Upper Cp', 'Lower Cp')
 title('Coefficient of Pressure vs Normalized Chord')
 subtitle('Stall Angle 15 m/s')
 hold off
 
 
-
-
 % Coefficient of Lift vs Angle of Attack
 subplot(3,3,7:9);
-plot(Calculated_Values(:,1) , Calculated_Values(:,5));
+plot(Calculated_Values15(:,1) , Calculated_Values15(:,5));
 hold on
 plot(NACA(1:7,1) , NACA(1:7,2));
 legend('Student Cl','NACA Cl')
 title('AoA vs Coefficient of Lift');
 
+
+
+
+
+
+
+
+
+%%Plots for 30m/s
+% Velocity vs normalized chord (x/c)
+% 6 degrees
+figure;
+subplot(3,3,1);
+plot(VelUpper30(1:9, 1), VelUpper30(1:9, 22), 'r')
+hold on
+plot(VelLower30(1:8, 1), VelLower30(1:8, 22), 'b')
+legend('Upper Velocity', 'Lower Velocity')
+title('Velocity vs Normalized Chord')
+subtitle('6 Degree AoA 30 m/s')
+hold off
+
+% 0 Lift
+subplot(3, 3, 2);
+plot(VelUpper30(1:9, 1), VelUpper30(1:9, ZeroLiftIndex30 + 1), 'r')
+hold on
+plot(VelLower30(1:8, 1), VelLower30(1:8, ZeroLiftIndex30 + 1), 'b')
+legend('Upper Velocity', 'Lower Velocity')
+title('Velocity vs Normalized Chord')
+subtitle('Zero Lift 30 m/s')
+
+hold off
+
+% Stall 10 degrees 
+subplot(3, 3, 3);
+plot(VelUpper30(1:9, 1), VelUpper30(1:9, 26), 'r')
+hold on
+plot(VelLower30(1:8, 1), VelLower30(1:8, 26), 'b')
+legend('Upper Velocity', 'Lower Velocity')
+title('Velocity vs Normalized Chord')
+subtitle('Stall Angle 30 m/s')
+hold off
+
+
+% Coefficient of Pressure vs normalized chord (x/c)
+%6 degrees 
+subplot(3,3,4);
+plot(CpUpper30(1:9, 1), CpUpper30(1:9, 22), 'r')
+hold on
+plot(CpLower30(1:8, 1), CpLower30(1:8, 22), 'b')
+legend('Upper Cp', 'Lower Cp')
+title('Coefficient of Pressure vs Normalized Chord')
+subtitle('6 Degree AoA 30 m/s')
+hold off
+
+%Zero lift
+subplot(3,3,5);
+plot(CpUpper30(1:9, 1), CpUpper30(1:9, ZeroLiftIndex30 + 1), 'r')
+hold on
+plot(CpLower30(1:8, 1), CpLower30(1:8, ZeroLiftIndex30 + 1), 'b')
+legend('Upper Cp', 'Lower Cp')
+title('Coefficient of Pressure vs Normalized Chord')
+subtitle('Zero Lift 30 m/s')
+
+%Stall
+subplot(3,3,6);
+plot(CpUpper30(1:9, 1), CpUpper30(1:9, 26), 'r')
+hold on
+plot(CpLower30(1:8, 1), CpLower30(1:8, 26), 'b')
+legend('Upper Cp', 'Lower Cp')
+title('Coefficient of Pressure vs Normalized Chord')
+subtitle('Stall Angle 30 m/s')
+hold off
+
+
+% Coefficient of Lift vs Angle of Attack
+subplot(3,3,7:9);
+plot(Calculated_Values30(:,1) , Calculated_Values30(:,5));
+hold on
+plot(NACA(1:7,1) , NACA(1:7,2));
+legend('Student Cl','NACA Cl')
+title('AoA vs Coefficient of Lift');
